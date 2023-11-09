0c2f9fa1dd5ae965c4192885c3ab699e84526cb0
Wata <forgotoffcom@gmail.com>
2023-11-09 12:00:48 +0800

    Initial commit

---
+++ Ratata.py
@@ -0,0 +1,8 @@
+class Rat:
+    def __init__(self):
+        self.health = 100
+        self.filth = 50
+        self.size = 1
+        self.hunger = 70
+        self.rat_gang = []
+        self.belongings = {}
